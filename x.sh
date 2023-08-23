517fbe961809ac56c95fbecacf5b6b85de07cf4a
Hmiwy <53623487+Hmiwy@users.noreply.github.com>
2023-08-23 21:06:52 +0800

    Update x.sh

--- x.sh
+++ x.sh
@@ -1,12 +1,38 @@
-#!/bin/sh
-cd /root
-wget https://github.com/vaxilu/x-ui/releases/download/0.3.2/x-ui-linux-amd64.tar.gz  > /dev/null 2>&1
-tar zxvf x-ui-linux-amd64.tar.gz > /dev/null 2>&1
-cd x-ui > /dev/null 2>&1
-read -p "请输入要设置xui的端口： " port
-./x-ui setting -port $port
-echo "账号密码默认admin"
-nohup ./x-ui > /dev/null 2>&1 &
+#!/bin/bash
+wget https://github.com/vaxilu/x-ui/releases/download/0.3.2/x-ui-linux-amd64.tar.gz
+tar zxvf x-ui-linux-amd64.tar.gz
 
+cd /root/x-ui  
+
+read -p "请输入xui端口: " port
+
+./x-ui  setting -port $port  
+
+touch /etc/systemd/system/ui.service 
+
+cat>/etc/systemd/system/ui.service<< EOF
+[Unit]
+Description=x-ui Service
+After=network.target
+Wants=network.target
+
+[Service]
+Type=simple
+WorkingDirectory=/root/x-ui/
+ExecStart=/root/x-ui/x-ui
+Restart=on-failure
+RestartSec=30s
+RestartPreventExitStatus=23
+LimitNPROC=10000
+LimitNOFILE=1000000
+
+[Install]
+WantedBy=multi-user.target
+EOF
+chmod 777 /etc/systemd/system/ui.service    
+systemctl enable ui.service  
+systemctl start ui.service    
+systemctl restart ui.service   
+rm -rf /root/x-ui-linux-amd64.tar.gz
 
 
