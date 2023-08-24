41d74f93d9a2ce304952788f00addbc7e82fc6a0
Hmiwy <53623487+Hmiwy@users.noreply.github.com>
2023-08-24 22:10:09 +0800

    Update tz.sh

--- tz.sh
+++ tz.sh
@@ -33,7 +33,7 @@ if test -z "$replace2_text"
 then
 a=5555
 sed -i "s/-gg/$a/g" /etc/systemd/system/nezha.service
-echo $replance2_text
+echo $a
 else
 sed -i "s/-gg/$replace2_text/g" /etc/systemd/system/nezha.service
 fi
